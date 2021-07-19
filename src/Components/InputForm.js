827f4ad45cdc33722c190c9b46a8969fdefaa639
Owahed <owahedhossain@gmail.com>
2021-07-19 18:19:46 +0600

    first commit

---
+++ src/Components/InputForm.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import ShowList from "./ShowList";
+
+const InputForm = () => {
+    // Form Input
+
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const onSubmit = data => {
+        const eventData = {
+            firstName: data.firstName,
+            lastName: data.lastName,
+            email: data.email,
+            counter: data.counter,
+          };
+
+          const url = `http://localhost:5000/addForm`;
+
+          console.log(eventData);
+          fetch(url, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(eventData)
+          })
+            .then(res => console.log('server', res))
+        alert("Form Submit Successful")
+    };
+
+    //Show List
+    
+    return (
+        <div>
+            <div className="mt-5">
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <input placeholder="First Name" {...register("firstName")} />
+                    <br />
+                    <input className="my-3" placeholder="Last Name" {...register("lastName", { required: true })} />
+                    <br />
+                    <input placeholder="Email" type="email" {...register("email", { required: true })} />
+                    <br />
+                    <input className="my-3" placeholder="Counter"  {...register("counter", { required: true })} />
+                    <br />
+                    <input className="btn btn-info" type="submit" />
+                </form>
+            </div>
+
+            <div>
+                <ShowList />
+            </div>
+        </div>
+    );
+};
+
+export default InputForm;
---
+++ src/Components/ShowList.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles({
+    table: {
+        minWidth: 650,
+    },
+});
+
+
+const ShowList = () => {
+    const [services, setServices] = useState([]);
+    console.log(services)
+
+    useEffect(() => {
+        fetch('http://localhost:5000/allList')
+            .then(res => res.json())
+            .then(data => setServices(data));
+    }, []);
+
+    const classes = useStyles();
+    return (
+        <div className="container">
+            
+            <div className="mt-5">
+                <h3 className="mb-5 text-center">All Order List</h3>
+                <TableContainer component={Paper}>
+                    <Table className={classes.table} aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell><b>Name</b></TableCell>
+                                <TableCell align="right"> <b>Email ID</b> </TableCell>
+                                <TableCell align="right"> <b>Country</b>  </TableCell>
+                                
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {services.map((row) => (
+                                <TableRow key={row.name}>
+                                    <TableCell component="th" scope="row">
+                                    {row.firstName}  {row.lastName}
+                                    </TableCell>
+
+                                    <TableCell align="right">{row.email}</TableCell>
+                                    <TableCell align="right">{row.counter}</TableCell>
+                                   
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </div>
+        </div>
+    );
+};
+
+export default ShowList;
